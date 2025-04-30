3179a38db7afbf15f463fac63dfd32a04fcd139c
mahmoudrezk29 <mahmoud.rezk22@hotmail.com>
2025-04-30 23:54:32 +0400

    pytest after cleaning

--- model/model.py
+++ model/model.py
@@ -41,14 +41,14 @@ def compute_model_metrics(y, preds):
     recall : float
     fbeta : float
     """
-    fbeta = fbeta_score(y, preds, 
-                        beta=1, 
+    fbeta = fbeta_score(y, preds,
+                        beta=1,
                         zero_division=1)
-    precision = precision_score(y, 
-                                preds, 
+    precision = precision_score(y,
+                                preds,
                                 zero_division=1)
-    recall = recall_score(y, 
-                          preds, 
+    recall = recall_score(y,
+                          preds,
                           zero_division=1)
     return precision, recall, fbeta
 
